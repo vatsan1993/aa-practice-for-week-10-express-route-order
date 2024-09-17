02167a0380ecc922d131563b45a9856294a01d4a
Srivatsan <vatsan1993@gmail.com>
2024-09-17 16:50:22 +0530

    finished

--- server/app.js
+++ server/app.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Server is alive");
+  res.send('Server is alive');
 });
 
 app.get('/hello', (req, res) => {
-    res.send("Hello, my friend!");
-});
-
-app.get(['/goodbye', '/goodbye/*'], (req, res) => {
-    res.send("Goodbye, my friend!");
-});
-
-app.get('/goodbye/until/:time', (req, res) => {
-    res.send(`Goodbye. See you ${req.params.time}.`);
+  res.send('Hello, my friend!');
 });
 
 app.get('/goodbye/until/forever', (req, res) => {
-    res.send("So long. Farewell. Have a great life!");
+  res.send('So long. Farewell. Have a great life!');
+});
+
+app.get('/goodbye/until/:time', (req, res) => {
+  res.send(`Have a nice day! See you ${req.params.time}.`);
+});
+
+app.get(['/goodbye', '/goodbye/*'], (req, res) => {
+  res.send('Goodbye, my friend!');
 });
 
 const port = 5000;
